741b4f08f6633643ed4169e45d7a53097ab8a875
Tyson <deppravacion@gmail.com>
2023-06-30 14:58:12 -0600

    commerce

---
+++ src/components/cart/Confirmation.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Confirmation = (props) => {
+
+
+
+  return(
+    <>
+    <div className="cartLeftBlockBody">
+
+    <div className="confirmation">
+      <div>
+        <iframe 
+          // width="560" 
+          // height="315" 
+          width='500'
+          height='281'
+          src="https://www.youtube.com/embed/ifJctID1zUc" 
+          title="YouTube video player" frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowfullscreen>
+        </iframe>
+      </div>
+      <div>
+        <h2>Confirmation Complete! </h2>
+      </div>   
+    </div>
+    </div>
+    </>
+  )
+}
